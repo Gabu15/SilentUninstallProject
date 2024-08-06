840104dedb0dbb578e29d958d2c6ca0bdbef22f6
Gabu15 <35672564+Gabu15@users.noreply.github.com>
2024-08-05 20:33:46 -0400

    Add files via upload

---
+++ Bentley Systems/Openroad Designer/ORD_Conceptstation_CE.bat
@@ -0,0 +1,2 @@
+:: uninstall ORD ConceptStation CE
+"C:\ProgramData\Package Cache\{d703e6ce-f505-4915-adb2-13e97ededcaf}\Setup_OpenRoadsConceptStationx64_10.00.16.84.exe"  /uninstall /norestart /quiet
---
+++ Bentley Systems/Openroad Designer/ORD_SignCAD.ps1
@@ -0,0 +1,2 @@
+# Uninstall all ORD SignCAD applications
+Get-Package -Name "OpenRoads SignCAD*" -AllVersions | Uninstall-Package -Force 
